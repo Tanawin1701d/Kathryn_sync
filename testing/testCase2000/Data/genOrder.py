f0c8b66f73fb1541b81d9411484d8824663c8a53
tanawin <umd-dev01-connect@outlook.com>
2023-03-12 23:38:14 +0700

    add 2000 message generator

---
+++ testing/testCase2000/Data/genOrder.py
@@ -0,0 +1,9 @@
+import random
+import json
+import string
+
+
+with open(f'0.json', 'w') as f:
+        x = dict()
+        x["orders"] = [y for y in range(1, 2000)]
+        json.dump(x, f, indent=4)
